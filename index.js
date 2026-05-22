87a91679547678ef1a7f97e7882e282fefb96437
Roman <romanmendozamx@outlook.com>
2026-05-22 12:43:10 -0700

    Agregado la conexion con mongodb

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const pool = require('./db');
 const app = express();
+const connectMongoDB = require("./mongoConnection");
+
+
+app.use(express.json());
+connectMongoDB();
 
 app.use(express.json());
 
@@ -124,3 +129,57 @@ app.listen(3000, () => {
 });
 
 
+const Vehiculo = require("./Vehiculo");
+
+app.get("/api/getVehiculos", async (req, res) => {
+  try {
+    const vehiculos = await Vehiculo.find();
+
+    res.status(200).json({
+      message: "Vehículos consultados correctamente",
+      data: vehiculos,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al consultar vehículos",
+      error: error.message,
+    });
+  }
+});
+
+app.post("/api/createVehiculo", async (req, res) => {
+  try {
+    const { marca, modelo, anio, color } = req.body;
+
+    if (!marca || !modelo || !anio || !color) {
+      return res.status(400).json({
+        message: "Todos los campos son obligatorios",
+      });
+    }
+
+    if (isNaN(anio)) {
+      return res.status(400).json({
+        message: "El año debe ser numérico",
+      });
+    }
+
+    const nuevoVehiculo = new Vehiculo({
+      marca,
+      modelo,
+      anio,
+      color,
+    });
+
+    await nuevoVehiculo.save();
+
+    res.status(201).json({
+      message: "Vehículo creado correctamente",
+      data: nuevoVehiculo,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al crear vehículo",
+      error: error.message,
+    });
+  }
+});
